c3fe4ced4efde7f62d214dd26c6051109b66e78b
mmikhey <mila_miheiceva@mail.ru>
2016-10-09 21:14:32 +0300

    DO: implemented operation class and decision class

---
+++ Project/SchedulerTask-2.0/Test/Builder/OperationTest.cs
@@ -0,0 +1,24 @@
+﻿using Builder.Operation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test.Builder
+{
+    [TestClass]
+    public class OperationTest
+    {
+        public void SetUp()
+        {
+        }
+
+        public void TearDown()
+        { 
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+
+        }
+    }
+}
