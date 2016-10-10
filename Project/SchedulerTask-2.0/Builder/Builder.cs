782c657786293503837778204d83140f529e8d6a
alexdyogtev <alex.dyogtev@yandex.ru>
2016-10-10 21:11:06 +0300

    add base of Builder and DefaultBuilder

--- Project/SchedulerTask-2.0/Builder/Builder.cs
+++ Project/SchedulerTask-2.0/Builder/Builder.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Builder.Equipment;
+using Builder.Front;
+using Builder.Front.Building;
+using Builder.IO;
 
 namespace Builder
 {
@@ -11,9 +15,29 @@ namespace Builder
     /// </summary>
     public class Builder
     {
+        private string inputDir;
+        private string outputDir;
+
+        public Builder(string inputDir, string outputDir)
+        {
+            this.inputDir = inputDir;
+            this.outputDir = outputDir;
+        }
+
         public void Run()
         {
-            
+            Reader reader = new Reader(inputDir);
+
+            List<Party> partys;
+            Dictionary<int, IOperation> operations;
+            Dictionary<int, IEquipment> equipments;
+            reader.ReadData(out partys, out operations, out equipments);
+
+            FrontBuilding frontBuilding = new FrontBuilding(partys);
+            frontBuilding.Build();
+
+            Writer writer = new Writer(outputDir);
+            writer.WriteData(operations);
         }
     }
 }
--- Project/SchedulerTask-2.0/Builder/Front/Building/DefaultBuilder.cs
+++ Project/SchedulerTask-2.0/Builder/Front/Building/DefaultBuilder.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Builder.Front.Sorting;
 
 namespace Builder.Front.Building
 {
@@ -13,14 +14,80 @@ namespace Builder.Front.Building
     public class DefaultBuilder : IBuilder
     {
         private List<Party> party;
-        private List<Operation> operations;
-        private EquipmentManager equipmentManager;
-        public DefaultBuilder(List<Party> party, EquipmentManager equipmentManager)
+        private List<IOperation> operations;
+        private ISorter frontSorter;
+
+        public DefaultBuilder(List<Party> party, /*EquipmentManager equipmentManager*/ ISorter frontSorter)
         {
-    }
+            this.party = party;
+
+            // Получение операций из партий
+            operations = new List<IOperation>();
+            foreach (Party i in party)
+            {
+                TreeIterator partyIterator = i.getIterator(i);
+                while (partyIterator.next())
+                {
+                    operations.AddRange(partyIterator.Current.getPartyOperations());
+                }
+            }
+
+            //this.equipmentManager = equipmentManager;
+            this.frontSorter = frontSorter;
+        }
+
         public void Build()
         {
-            
+            List<DateTime> events = new List<DateTime>();
+            foreach (Party i in party)
+            {
+                events.Add(new DateTime(i.getStartTimeParty()));
+            }
+
+            while (events.Count != 0)
+            {
+                List<IOperation> front = new List<IOperation>();
+
+                // Формирование фронта
+                foreach (IOperation operation in operations)
+                {
+                    if (!operation.IsEnabled() && operation.PreviousOperationIsEnd(events[0]) &&
+                        DateTime.Compare(operation.GetParty().getStartTimeParty(), events[0]) <= 0)
+                    {
+                        DateTime operationTime;
+                        SingleEquipment equipment;
+                        if (EquipmentManager.IsFree(events[0], operation, out operationTime, out equipment))
+                        {
+                            front.Add(operation);
+                        }
+                        else
+                        {
+                            events.Add(operationTime);
+                        }
+                    }
+                }
+
+                // Сортировка фронта
+                frontSorter.Sort(front);
+
+                // Назначение операций
+                foreach (IOperation operation in front)
+                {
+                    DateTime operationTime;
+                    SingleEquipment equipment;
+
+                    if (EquipmentManager.IsFree(events[0], operation, out operationTime, out equipment))
+                    {
+                        operation.SetOperationInPlan(events[0], operationTime, equipment);
+                        equipment.OccupyEquip(events[0], operationTime);
+                    }
+
+                    events.Add(operationTime);
+                }
+
+                events.RemoveAt(0);
+            }
+
         }
     }
 }
