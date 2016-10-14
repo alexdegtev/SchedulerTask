36f2e99cdc080021e072ee8d3440e302b66262f0
yolosora <kozh.sora@mail.ru>
2016-10-14 18:10:35 +0300

    Update SingleEquipment.cs

--- Project/SchedulerTask-2.0/Builder/Equipment/SingleEquipment.cs
+++ Project/SchedulerTask-2.0/Builder/Equipment/SingleEquipment.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +9,8 @@ namespace Builder.Equipment
     /// <summary>
     /// Implement IEquipment
     /// </summary>
-    public class SingleEquipment
+    public class SingleEquipment : IEquipment
     {
-
+        public int GetID();
     }
 }
