0d678ef24b0a0d9ea30ab1b964b6e26486668979
Alexey Kornilov <koreshock@gmail.com>
2016-10-21 20:14:42 +0300

    Сhange in architecture

---
+++ Project/SchedulerTask-2.0/BuilderConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderConsole
+{
+    /// <summary>
+    /// Клиентский код для построителя.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuilderScheduler builder = new BuilderScheduler("D:/SchedulerTask-2.0/SchedulerTask/Project/SchedulerTask-2.0/TestData/TestDataBuilder/test1/", "D:/SchedulerTask-2.0/SchedulerTask/Project/SchedulerTask-2.0/TestData/TestDataBuilder/test1/");
+            builder.Run();
+        }
+    }
+}
