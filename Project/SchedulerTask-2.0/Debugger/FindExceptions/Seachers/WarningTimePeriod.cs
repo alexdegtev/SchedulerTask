e039f61bc4bea40280cca0c38e21c88fc08cc1b2
yolosora <kozh.sora@mail.ru>
2016-11-18 03:39:05 +0300

    Update WarningTimePeriod.cs

--- Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/WarningTimePeriod.cs
+++ Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/WarningTimePeriod.cs
@@ -1,21 +1,41 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using Debugger.Exceptions;
 using CommonTypes;
+using CommonTypes.Operation;
+using CommonTypes.Decision;
 
 namespace Debugger.FindExceptions.Seachers
 {
     class WarningTimePeriod : IExceptionSearch
     {
+        Dictionary<int, IOperation> operations;
+        List<IDecision> decisions;
         // Конструктор
-        public WarningTimePeriod()
+        public WarningTimePeriod(Dictionary<int, IOperation> operations, List<IDecision> decisions)
         {
+            this.operations = operations;
+            this.decisions = decisions;
         }
 
         public List<IException> Execute()
         {
             ConsoleLogger.Log("Ищем нарушения директивных сроков...");
             List<IException> exceptions = new List<IException>();
+           
+            foreach (var decision in decisions)
+            {
+                if (decision.GetEndTime() < operations[decision.GetOperation().GetId()].GetParty().GetEndTimeParty())
+                {
+                    
+                    exceptions.Add(new Exception("D00",
+                                                 "Warning",
+                                                 "В построенном расписании имеется нарушение директивного срока",
+                                                 decision.ToString(),
+                                                 ""));
+                }
+            }
 
+            
             return exceptions;
         }
     }
