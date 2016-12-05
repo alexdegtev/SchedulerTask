7fdab407fd1f48d45bb1e4b7dc1ed75d64a4acc5
VolkovVadim <VolkovVadim5@gmail.com>
2016-12-05 23:32:16 +0300

    Added new exceptions classes

--- Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionEquipment.cs
+++ Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionEquipment.cs
@@ -1,20 +1,60 @@
 ﻿using System.Collections.Generic;
 using Debugger.Exceptions;
+using CommonTypes.Decision;
+using CommonTypes.Operation;
+using CommonTypes.Equipment;
 using CommonTypes;
 
 namespace Debugger.FindExceptions.Seachers
 {
     class ExceptionEquipment : IExceptionSearch
     {
-        public ExceptionEquipment()
+        Dictionary<int, IOperation> operations;
+        List<IDecision> decisions;
+
+        public ExceptionEquipment(Dictionary<int, IOperation> operations, List<IDecision> decisions)
         {
             // Конструктор
+            this.operations = operations;
+            this.decisions = decisions;
         }
 
         public List<IException> Execute()
         {
             ConsoleLogger.Log("Ищем ошибки в соответствии оборудования операциям...");
             List<IException> exceptions = new List<IException>();
+
+            foreach(var decision in decisions)
+            {
+                int singleEquipmentId = decision.GetEquipment().GetId();
+                int groupEquipmentId  = decision.GetEquipment().GetParentGroupId();
+                
+                // Ищем соответветствующую операцию в исходных данных
+                foreach(var operation in operations)
+                {
+                    // Нашли соответветствующую операцию в исходном расписании
+                    if(decision.GetOperation() == operation.Value)
+                    {
+                        // Находим номер оборудования или группы оборудования, на котором должна выполняться операция
+                        int operationEquipmentId = operation.Value.GetEquipment().GetId();
+                        bool isCorrect = false;
+
+                        if (singleEquipmentId == operationEquipmentId || groupEquipmentId == operationEquipmentId)
+                            isCorrect = true;
+                        else
+                            isCorrect = false;
+
+                        if(!isCorrect)
+                        {
+                            exceptions.Add(new Debugger.Exceptions.Exception("V01",
+                                                 "Error",
+                                                 "Несоответствие оборудования и выполняемой на нем операции",
+                                                 "",
+                                                 ""));
+                        }
+                    }
+                }
+            }
 
             return exceptions;
         }
--- Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionOperationDuration.cs
+++ Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionOperationDuration.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections.Generic;
+using System;
 using Debugger.Exceptions;
+using CommonTypes.Decision;
+using CommonTypes.Operation;
 using CommonTypes;
 
 namespace Debugger.FindExceptions.Seachers
 {
     class ExceptionOperationDuration : IExceptionSearch
     {
-        public ExceptionOperationDuration()
+        Dictionary<int, IOperation> operations;
+        List<IDecision> decisions;
+
+        public ExceptionOperationDuration(Dictionary<int, IOperation> operations, List<IDecision> decisions)
         {
             // Конструктор
+            this.operations = operations;
+            this.decisions = decisions;
         }
 
         public List<IException> Execute()
         {
             ConsoleLogger.Log("Ищем ошибки в длительностях операций...");
             List<IException> exceptions = new List<IException>();
+            bool inFirstInterval = false;
+            bool inLastInterval  = false;
+
+            foreach(var decision in decisions)
+            {
+                TimeSpan realtime = decision.GetEquipment().GetCalendar().GetRealWorkTime(
+                    decision.GetStartTime(), decision.GetEndTime(), out inFirstInterval, out inLastInterval);
+                TimeSpan duration = decision.GetOperation().GetDuration();
+                
+                if(realtime < duration)
+                {
+                    exceptions.Add(new Debugger.Exceptions.Exception("V03",
+                                                 "Error",
+                                                 "Несоответствие набора исходных операций операциям в расписании",
+                                                 "",
+                                                 ""));
+                }
+            }
 
             return exceptions;
         }
