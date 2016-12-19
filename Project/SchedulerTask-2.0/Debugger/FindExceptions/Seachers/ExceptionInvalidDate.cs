0603690368ec4f4ae12c775d2b69aad220696fe0
VolkovVadim <VolkovVadim5@gmail.com>
2016-12-19 22:31:07 +0300

    Add invalid date exception

--- Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionInvalidDate.cs
+++ Project/SchedulerTask-2.0/Debugger/FindExceptions/Seachers/ExceptionInvalidDate.cs
@@ -1,20 +1,56 @@
 ﻿using System.Collections.Generic;
 using Debugger.Exceptions;
+using CommonTypes.Decision;
+using CommonTypes.Operation;
+using CommonTypes.Equipment;
 using CommonTypes;
 
 namespace Debugger.FindExceptions.Seachers
 {
     class ExceptionInvalidDate : IExceptionSearch
     {
-        public ExceptionInvalidDate()
+        Dictionary<int, IOperation> operations;
+        List<IDecision> decisions;
+
+        public ExceptionInvalidDate(Dictionary<int, IOperation> operations, List<IDecision> decisions)
         {
             // Конструктор
+            this.operations = operations;
+            this.decisions = decisions;
         }
 
         public List<IException> Execute()
         {
             ConsoleLogger.Log("Ищем ошибки в назначении времени выполнения операций...");
             List<IException> exceptions = new List<IException>();
+
+            foreach(var decision in decisions)
+            {
+                System.DateTime start = decision.GetStartTime();
+                System.DateTime finish = decision.GetEndTime();
+                bool isWorkTime = false;
+
+                decision.GetEquipment().GetCalendar().FindInterval(start, out isWorkTime);
+                if(!isWorkTime)
+                {
+                    exceptions.Add(new Exception("R03",
+                                                 "Error",
+                                                 "Начало выполнения операции в недоступное время для оборудования",
+                                                 "Номер оборудования : " + decision.GetEquipment().GetId(),
+                                                 "Номер операции : " + decision.GetOperation().GetId()));
+                }
+
+                decision.GetEquipment().GetCalendar().FindInterval(finish, out isWorkTime);
+                if (!isWorkTime)
+                {
+                    exceptions.Add(new Exception("R03",
+                                                 "Error",
+                                                 "Завершение операции в недоступное время для оборудования ",
+                                                 "Номер оборудования : " + decision.GetEquipment().GetId(),
+                                                 "Номер операции : " + decision.GetOperation().GetId()));
+                }
+
+            }
 
             return exceptions;
         }
